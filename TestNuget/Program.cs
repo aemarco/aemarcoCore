ce877fe4ad93a8e05f89420ea9847a608374ef47
aemarco <4aemarco@gmail.com>
2017-12-31 19:15:08 +0100

    updated

--- TestNuget/Program.cs
+++ TestNuget/Program.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TestNuget
+﻿namespace TestNuget
 {
     class Program
     {
         static void Main(string[] args)
         {
-            aemarcoCore.Crawlers.Ftop_Wallpaper wall = new aemarcoCore.Crawlers.Ftop_Wallpaper();
+            //aemarcoCore.Crawlers.Ftop_Wallpaper wall = new aemarcoCore.Crawlers.Ftop_Wallpaper();
 
-            
+
         }
     }
 }
