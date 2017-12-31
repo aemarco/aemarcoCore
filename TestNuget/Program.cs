26de69a98aceb2c263c369adc828d1f88a964f8c
aemarco <4aemarco@gmail.com>
2017-12-31 05:38:05 +0100

    started with nuget

---
+++ TestNuget/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNuget
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            aemarcoCore.Crawlers.Ftop_Wallpaper wall = new aemarcoCore.Crawlers.Ftop_Wallpaper();
+
+            
+        }
+    }
+}
