e11a48813f21dd244a57dc92c47098c2db537100
aemarco <aemarco>
2023-07-28 00:50:49 +0200

    refactor and fix zoompussy

--- aemarco.Crawler.Wallpaper/Crawlers/Zoompussy.cs
+++ aemarco.Crawler.Wallpaper/Crawlers/Zoompussy.cs
@@ -1,25 +1,20 @@
 ﻿// ReSharper disable UnusedType.Global
 namespace aemarco.Crawler.Wallpaper.Crawlers;
 
-//TODO modernize
 [WallpaperCrawler("Zoompussy")]
 internal class Zoompussy : WallpaperCrawlerBasis
 {
-    private readonly Uri _uri = new("http://zoompussy.com/");
 
-
+    private readonly Uri _uri = new("https://zoompussy.com");
     public Zoompussy(
         int startPage,
         int lastPage,
         bool onlyNews)
         : base(startPage, lastPage, onlyNews)
-    {
-
-    }
+    { }
 
     protected override List<CrawlOffer> GetCrawlsOffers()
     {
-        var result = new List<CrawlOffer>();
         var cats = new List<string>
         {
             "asian",
@@ -40,78 +35,64 @@ internal class Zoompussy : WallpaperCrawlerBasis
             "stockings"
         };
 
-        foreach (var cat in cats)
-        {
-            var cc = GetContentCategory(cat);
-            if (cc is null)
-                continue;
+        var result = cats
+            .Select(cat =>
+                GetContentCategory(cat) is { } cc
+                    ? CreateCrawlOffer(cat, new PageUri(new Uri(_uri, $"search/{cat}/")), cc)
+                    : null)
+            .OfType<CrawlOffer>()
+            .ToList();
 
-            result.Add(CreateCrawlOffer(
-                cat,
-                new Uri(_uri, $"/search/{cat}/")!,
-                cc));
-        }
         return result;
     }
-    protected override PageUri GetSiteUrlForCategory(CrawlOffer catJob)
-    {
-        //z.B. "http://zoompussy.com/search/asian/page/1/"
-        return new Uri($"{catJob.CategoryUri.Uri.AbsoluteUri}page/{catJob.CurrentPage}")!;
-    }
-    protected override string GetSearchStringGorEntryNodes()
-    {
-        return "//li/div[@class='thumb']/a";
-    }
+    //z.B. "http://zoompussy.com/search/asian/page/1/"
+    protected override PageUri GetSiteUrlForCategory(CrawlOffer catJob) =>
+        catJob.CategoryUri.WithHref($"page/{catJob.CurrentPage}");
+    protected override string GetSearchStringGorEntryNodes() =>
+        "//li/div[@class='thumb']/a";
     protected override ContentCategory DefaultCategory => new(Category.Girls);
     protected override bool AddWallEntry(PageNode pageNode, CrawlOffer catJob)
     {
-        var node = pageNode.Node;
-        var source = new WallEntrySource(_uri, pageNode, catJob.Category, catJob.SiteCategoryName);
-
-        //docs
-        source.DetailsDoc = source.GetChildDocumentFromRootNode();
-        if (source.DetailsDoc is null)
+        //navigate
+        if (pageNode
+                .GetHref()?
+                .Navigate() is not { } detailsPage)
         {
-            AddWarning($"Could not read DetailsDoc from node {node.InnerHtml}");
+            AddWarning(pageNode, "Could not find DetailsDoc");
+            return false;
+        }
+        if (detailsPage
+                .FindNode("//div[@id='post_content']/blockquote/a")?
+                .GetHref() is not { } imageUri)
+        {
+            AddWarning(detailsPage, "Could not get ImageUri");
             return false;
         }
 
         //details
-        source.ImageUri = source.GetUriFromDocument(source.DetailsDoc, "//div[@id='post_content']/blockquote/a", "href");
-
-        if (source.ImageUri is null)
+        var source = new WallEntrySource(_uri, pageNode, catJob.Category, catJob.SiteCategoryName)
         {
-            AddWarning($"Could not get ImageUri from node {source.DetailsDoc.DocumentNode.InnerHtml}");
-            return false;
-        }
-        source.ThumbnailUri = source.GetUriFromDocument(source.DetailsDoc, "//div[@id='post_content']/blockquote/a/img", "src");
+            ImageUri = imageUri,
+            ThumbnailUri = detailsPage
+                .FindNode("//div[@id='post_content']/blockquote/a/img")?
+                .GetSrc()
+        };
         source.SetFilenamePrefix(catJob.SiteCategoryName);
-        source.Tags = WallEntrySource.GetTagsFromNodes(
-            source.DetailsDoc,
-            "//div[@class='post_z']/a",
-            x =>
-            {
-                if (x.Attributes["href"] != null &&
-                    x.Attributes["href"].Value.StartsWith(_uri.AbsoluteUri))
-                {
-                    return WebUtility.HtmlDecode(x.InnerText).Trim();
-                }
-                return null;
-            });
+        source.AddTagsFromInnerTexts(
+            detailsPage
+                .FindNodes("//div[@class='post_z']/a")
+                .Where(x => x.GetHref()?.Uri is { } link && link.Host == _uri.Host));
 
-
-        var wallEntry = source.WallEntry;
-        if (wallEntry is null)
+        //entry
+        if (source.ToWallEntry() is not { } entry)
             return false;
 
-
-        //damn.... no fun without referer --> special treatment needed :(
-        //source.DownloadWithReferer(wallEntry, $"{_uri.AbsoluteUri}{wallEntry.FileName.ToLower()}");
-        //if (!wallEntry.IsValid || string.IsNullOrWhiteSpace(wallEntry.FileContentAsBase64String)) 
-        //    return false;
-
-
-        AddEntry(wallEntry, catJob);
+        AddEntry(entry, catJob);
         return true;
     }
 }
+
+//damn.... no fun without referer --> special treatment needed :(
+//source.DownloadWithReferer(wallEntry, $"{_uri.AbsoluteUri}{wallEntry.FileName.ToLower()}");
+//if (!wallEntry.IsValid || string.IsNullOrWhiteSpace(wallEntry.FileContentAsBase64String)) 
+//    return false;
